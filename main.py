ef657c37671320ab9711e1c7948c3ecf8b9fc24c
SzyMan1337 <szymanski.mateusz@gmail.com>
2021-04-25 16:25:48 +0200

    Correct reaction for incorrect username.

--- main.py
+++ main.py
@@ -9,22 +9,30 @@ def hello():
     return {"List all user repositories with ratings": "/repositories/username", "Display sum of all user's repositories ratings":"/rating/username"}
 
 def get_json_of_user(username):
-    request = requests.get('https://api.github.com/users/'+username+'/repos')
-    repo_json = request.json()
-    return repo_json
+    response = requests.get('https://api.github.com/users/'+username+'/repos')
+    if response.ok:
+        repo_json = response.json()
+        return repo_json, True
+    
+    return {}, False
 
 @app.get("/rating")
 def rating(username: str = ""):
-    repo_json = get_json_of_user(username)
-    count = 0
-    for i in range(0,len(repo_json)):
-        count += repo_json[i]['stargazers_count']
-    return {"rating": count}
+    repo_json,status = get_json_of_user(username)
+    if status:
+        count = 0
+        for i in range(0,len(repo_json)):
+            count += repo_json[i]['stargazers_count']
+        return {"rating": count}
+    return {"Incorrect username": f"{username}"}
 
 @app.get("/repositories")
 def list_repo(username: str = ""):
-    repo_json = get_json_of_user(username)
-    jss = {}
-    for i in range(0,len(repo_json)):
-        jss[repo_json[i]['name']] = repo_json[i]['stargazers_count']
-    return jss
+    repo_json,status = get_json_of_user(username)
+    if status:
+        jss = {}
+        for i in range(0,len(repo_json)):
+            jss[repo_json[i]['name']] = repo_json[i]['stargazers_count']
+        return jss
+    return {"Incorrect username": f"{username}"}
+
